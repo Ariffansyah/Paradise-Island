ffa843b661c4686bb92283026a92fb0dce078551
arpthef <harimato9@gmail.com>
2024-10-09 10:40:36 +0700

    adding vercel anlytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Music from './components/Music';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
       <Navbar />
       <Music />
+      <Analytics />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
